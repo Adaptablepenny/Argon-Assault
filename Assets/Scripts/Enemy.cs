bc18f4b1dc8080ca525b87372a30c6e0eaf89d8e
Michael Rosique <michaelrosique@gmail.com>
2019-10-20 23:19:44 -0500

    Added explosion VFX and SFX to enemy ships by instantiating the prefab when collided with a particle

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject deathFX;
+    // Start is called before the first frame update
+    Collider col;
+    void Start()
+    {
+        gameObject.AddComponent<MeshCollider>().convex = true;
+        col.isTrigger.Equals(false);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnParticleCollision(GameObject enemy)
+    {
+        Instantiate(deathFX,transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }    
+}
