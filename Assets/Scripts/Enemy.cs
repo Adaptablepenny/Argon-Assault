652c3ffeb97f4c21de5a229b98a6d000d055b5ef
Michael Rosique <michaelrosique@gmail.com>
2019-10-21 11:00:12 -0500

    Added a ScoreBoard to the game via a canvas and a ScoreBoard.cs each hit adds 12 points as of now

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,26 +4,43 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-
+    
     [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
+
+
     // Start is called before the first frame update
-    Collider col;
     void Start()
     {
-        gameObject.AddComponent<MeshCollider>().convex = true; //Adds a MeshCollider component to the enemy ship and makes it convex
-        col.isTrigger.Equals(false);// Sets the colliders trigger to false
+        AddMeshCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+
+    }
+
+    private void AddMeshCollider()
+    {
+        Collider col = gameObject.AddComponent<MeshCollider>(); //Adds a MeshCollider to the gameObject
+        gameObject.AddComponent<MeshCollider>().convex = true;//Adds convex to the mesh collider
+        col.isTrigger = false;// Sets the colliders trigger to false, this also keeps spitting on nullreferenceexceptions
         
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+ 
     }
 
     private void OnParticleCollision(GameObject enemy)
     {
-        Instantiate(deathFX,transform.position, Quaternion.identity);// Creates a Enemy Death FX prefab into the world
+        scoreBoard.AddScore(scorePerHit);
+        GameObject fx = Instantiate(deathFX,transform.position, Quaternion.identity);// Creates a Enemy Death FX prefab into the world
+        fx.transform.parent = parent;
         Destroy(gameObject);
+        
+
     }    
 }
