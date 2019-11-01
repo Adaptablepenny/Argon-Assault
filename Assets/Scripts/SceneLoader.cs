9955739903bea2163800cfb7ea26aa38adbec354
Michael Rosique <michaelrosique@gmail.com>
2019-11-01 09:52:24 -0500

    Changed the amount of time the splash screen lasts from 5 seconds to 3 seconds

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -8,7 +8,7 @@ public class SceneLoader : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadScene", 5f);
+        Invoke("LoadScene", 3f);
     }
         
     
